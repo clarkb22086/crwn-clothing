c575b35c82192c21c92d506316dd063103e1c913
smoldyr <clarkb@clarkb.com>
2021-02-11 19:54:03 -0600

    Formatted code

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import "./sign-in.component";
+import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
   constructor(props) {
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import React from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-
-import SHOP_DATA from './shop.data';
+import SHOP_DATA from "./shop.data";
 
 class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
 
-    render() {
-        const { collections } = this.state;
-        return (<div className='shop-page'>
-            {collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))}
-        </div>);
-    }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ShopPage;
